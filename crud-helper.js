95a25e71686ab62dea94c30d15adaa1612460d4f
Jessica <jessicamendez328@gmail.com>
2024-04-20 23:17:48 -0400

    added page level components

--- crud-helper.js
+++ crud-helper.js
@@ -4,10 +4,10 @@ require('./config/database');
 
 // Require the Mongoose models
 const User = require('./models/user');
-const Item = require('./models/item');
+const Product = require('./models/product');
 const Category = require('./models/category');
 const Order = require('./models/order');
 
 // Local variables will come in handy for holding retrieved documents
-let user, item, category, order;
+let user, product, category, order;
 let users, items, categories, orders;
