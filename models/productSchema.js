948e6bfe2eaa7f08deb4ea1b00efc62cb915b2b5
Jessica <jessicamendez328@gmail.com>
2024-04-22 16:14:55 -0400

    builind out admin profile functions

--- models/productSchema.js
+++ models/productSchema.js
@@ -2,7 +2,7 @@ const Schema = require('mongoose').Schema;
 
 const productSchema = new Schema({
   name: { type: String, required: true },
-  emoji: String,
+  emoji: { type: String, required: true },
   category: {type: Schema.Types.ObjectId, ref: 'Category'},
   subCategory: {type: Schema.Types.ObjectId, ref: 'SubCategory'},
   tags: [{type: Schema.Types.ObjectId, ref: 'Tag'}],
--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -2,12 +2,14 @@ import './ProfilePage.css';
 import { Link } from 'react-router-dom';
 // import Logo from '../../components/Logo/Logo';
 import { getUser } from '../../utilities/users-service';
+import AddProductForm from '../../components/AddProductForm/AddProductForm';
   
   export default function ProfilePage() {
   const user = getUser();
   return (
       <div className='profile'>
       <img className="profile-photo" src={user.urlImage} alt="profile" width="150" height="150" />
+      {/* <img className="profile-photo" src={user.urlImage} alt="profile" width="150" height="150" /> // icebox-profile-photo */}
       <br />
       <br />
       <div className='setting-profile-details'>
@@ -18,22 +20,20 @@ import { getUser } from '../../utilities/users-service';
               <strong>Full Name:</strong> {user.firsrname} {user.lastname}
       <br />
               <strong>Phone #:</strong> {user.customerphone}
-      <br />
-              <strong>Payment Details</strong> {user.paymentinfo}
       </div>
       <br />
-      <Link to="/profile">
-            <button>Edit my Profile</button>
+      <Link to="/inquiries">
+            <button>view my inquiry list</button>
           </Link>
       <br />
+
       <hr />
-      <br />
-      {user && user.role === 'Admin' ? (
+      {user && user.role === 'admin' ? (
+        <div className='admin-dashboard'>
+        <h3>Current Product Catalogue</h3>
         <div>
-          <br />
-        <h1>Vendor Portal</h1>
-          <br />
-        <Link to="/VendorAdminPage"><button>View my admin profile</button></Link>
+          <Link to="/add-product"><button>Add a new product</button></Link>
+        </div>
         </div>
         ) : (
         <div>
