b2997d6eff2f93f7f2f1d11585ce1a91cd79f604
Jessica <jessicamendez328@gmail.com>
2024-04-24 16:43:59 -0400

    some css clean up

--- src/components/CardProductDetail/CardProductDetail.jsx
+++ src/components/CardProductDetail/CardProductDetail.jsx
@@ -4,14 +4,16 @@ import { Link } from 'react-router-dom';
 
 const CardProductDetail = ({ product }) => {
   return (
-    <div className="product">
+    <div className="productCard">
       <Link to={`/products/${product._id}`}>
         <h2>{product.name}</h2>
       </Link>
+      <div className="cardDetails">
       <p>{product.emoji}</p>
       <p>Category: {product.category}</p>
       <p>Sub-Category: {product.subCategory}</p>
       <p>Price: ${product.price}</p>
+    </div>
     </div>
   );
 };
--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,15 +1,45 @@
+import React, { useState, useEffect } from 'react';
+import { index } from '../../utilities/products-api';
+import CardProductDetail from '../../components/CardProductDetail/CardProductDetail';
+import { Link } from 'react-router-dom';
 import './ShopPage.css';
-import { Link, useNavigate } from 'react-router-dom';
-import Logo from '../../components/Logo/Logo';
-import UserLogOut from '../../components/UserLogOut/UserLogOut';
 
-export default function ShopPage({ user, setUser }) {
-  const navigate = useNavigate();
+const ShopPage = () => {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const fetchedProducts = await index();
+        setProducts(fetchedProducts);
+        setLoading(false);
+      } catch (error) {
+        console.error('Error fetching products:', error);
+        setError('Failed to fetch products. Please try again later.');
+        setLoading(false);
+      }
+    };
+
+    fetchProducts();
+  }, []);
 
   return (
-    <main className="ShopPage">
-        <Logo />
-      <h2>inside the main</h2>
-    </main>
+    <div>
+      <h1>Shop</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p>{error}</p>}
+      <div className="product-grid">
+        {!loading &&
+          products.map(product => (
+            <Link key={product._id} to={`/products/${product._id}`}>
+              <CardProductDetail product={product} />
+            </Link>
+          ))}
+      </div>
+    </div>
   );
-}
+};
+
+export default ShopPage;
