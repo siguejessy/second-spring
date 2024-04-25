9cfec30bb60ca981534953ec6a2d8cba3157cbbc
Jessica <jessicamendez328@gmail.com>
2024-04-24 22:11:35 -0400

    about page & additional css added

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -12,12 +12,22 @@ export default function AboutPage({ user, setUser }) {
     <main className="AboutPage">
       <nav />
       <aside>
-        <Logo />
+        {/* <Logo /> */}
       </aside>
       <section>
-        <h2>About Project</h2>
-        <p>About Project</p>
-        {/* import reader-friendly ReadMe? */}
+        <h2>About this Project</h2>
+        <p>Welcome to Second Spring - your destination for thrifted treasures and unique finds for your home!
+        <br/>At Second Spring, we believe in giving new life to pre-loved items, allowing them to bloom once again in your space.
+        <br/>Our curated collection includes a variety of home decor, books, and interior design gems, each with its own story and charm. Whether you're searching for that perfect vintage vase, a cozy reading nook addition, or a statement piece to elevate your space, Second Spring has something for everyone.
+       <br/>
+        <br/>We're committed to sustainability and ethical shopping practices, offering a more eco-friendly alternative to traditional retail. By choosing Second Spring, you're not only supporting small businesses and local artisans, but also reducing waste and promoting a circular economy.
+       <br/>
+        <br/>The name "Second Spring" embodies our mission - to provide a fresh start for items, offering them a second chance to shine and bloom anew. It's all about embracing the beauty of renewal and transformation, where something old becomes something new.
+        <br/>
+        <br/>
+        While our current functionality may be streamlined due to time constraints, we're constantly evolving to better serve you. In future iterations, expect exciting features such as live chat with our admin for real-time assistance and seamless shopping experiences with add-to-cart and checkout functionality.
+        <br/>
+        <br/>Thank you for joining us on this journey of rediscovery and renewal. Let's make every season a Second Spring!</p>
       </section>
     </main>
   );
